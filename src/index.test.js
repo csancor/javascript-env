9077ec92d1d85b72e6334c4da90060bccf3a9700
csancor <cleitonsancor@hotmail.com>
2020-02-01 14:02:34 -0300

    Adicionando teste JSDom

--- src/index.test.js
+++ src/index.test.js
@@ -9,11 +9,12 @@ describe('My first test', () => {
 });
 
 describe('index.html', () => {
-  it('should say welcome', () => {
+  it('should say welcome', (done) => {
     const index = fs.readFileSync('./src/index.html', 'utf-8');
     jsdom.env(index, function(err, window){
       const h1 = window.document.getElementsByTagName('h1')[0];
       expect(h1.innerHTML).to.equal("welcome");
+      done();
       window.close();
     });
   });
