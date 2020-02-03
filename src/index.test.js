61ac8b9171d03de0e6c0c715057d194a72aa466b
csancor <cleitonsancor@hotmail.com>
2020-02-03 17:26:08 -0300

    Configurando API.

--- src/index.test.js
+++ src/index.test.js
@@ -9,11 +9,11 @@ describe('My first test', () => {
 });
 
 describe('index.html', () => {
-  it('should say welcome', (done) => {
+  it('should have h1 as Users', (done) => {
     const index = fs.readFileSync('./src/index.html', 'utf-8');
     jsdom.env(index, function(err, window){
       const h1 = window.document.getElementsByTagName('h1')[0];
-      expect(h1.innerHTML).to.equal("welcome");
+      expect(h1.innerHTML).to.equal("Users");
       done();
       window.close();
     });
